199bbf60aae074a0b888589c09ae303ce4ec271f
Vitor Carvalho <vdecarv@gmail.com>
2018-02-11 17:01:30 -0200

    Removing zoom on mobile

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -2,7 +2,7 @@
 <html lang="<?= site()->language() ? site()->language()->code() : 'en' ?>">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
   <title><?= $site->title()->html() ?></title>
   <meta name="description" content="<?= $site->description()->html() ?>">
   <?= css("https://unpkg.com/flickity@2/dist/flickity.min.css") ?>
